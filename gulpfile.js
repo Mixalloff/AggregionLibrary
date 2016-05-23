9003ff229bfb64d8a1c52319639c3baff249cf87
Mikhail Mikhalev <Mikhail Mikhalev>
2016-05-23 11:34:59 +0300

    Added gulp serve command for auto-build, server starting and live-reload

--- gulpfile.js
+++ gulpfile.js
@@ -2,26 +2,31 @@ var gulp  = require('gulp'),
     concat = require('gulp-concat'),
     mainBowerFiles = require('main-bower-files'),
     order = require('gulp-order'),
-    filter = require('gulp-filter');
+    filter = require('gulp-filter')
+    exec = require('child_process').exec;
 
-gulp.task('vendorsjs',function(){
-  return gulp.src(mainBowerFiles({
+// Компоненты bower
+var vendorsJsFiles = mainBowerFiles({
   filter:'**/*.js',
     paths: {
         bowerDirectory: 'bower_components'
     }
-}))
+});
+var vendorsCssFiles = mainBowerFiles({
+  filter:'**/*.css',
+    paths: {
+        bowerDirectory: 'bower_components'
+    }
+});
+
+gulp.task('vendorsjs',function(){
+  return gulp.src(vendorsJsFiles)
   .pipe(concat('vendors.js'))
   .pipe(gulp.dest('clientApp/compiled/js'));
 });
 
 gulp.task('vendorscss',function(){
-  return gulp.src(mainBowerFiles({
-  filter:'**/*.css',
-    paths: {
-        bowerDirectory: 'bower_components'
-    }
-}))
+  return gulp.src(vendorsCssFiles)
   .pipe(concat('vendors.css'))
   .pipe(gulp.dest('clientApp/compiled/css'));
 });
@@ -35,3 +40,25 @@ gulp.task('libraryjs',function(){
   .pipe(concat('libraryjs.js'))
   .pipe(gulp.dest('clientApp/compiled/js'));
 });
+
+gulp.task('server', function (cb) {
+  exec('node server.js', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
+})
+
+gulp.task('watch', function(){
+  gulp.watch(vendorsJsFiles, ['vendorsjs']);
+  gulp.watch(vendorsCssFiles, ['vendorscss']);
+  gulp.watch('clientApp/libraryApp/**/*.js', ['libraryjs']);
+});
+
+// Выполняет сборку
+gulp.task('build', ['vendorsjs', 'vendorscss', 'libraryjs']);
+
+// Собирает проект, запускае сервер и отслеживает изменения
+gulp.task('default', ['build', 'server', 'watch']);
+// Альтернативный вызов для запуска сервера и автосборки
+gulp.task('serve', ['default']);
